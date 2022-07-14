ff0b56fe12623543dc89fbba8283b7e4181a23ec
Felipe <felipedossantosflor@gmail.com>
2022-07-14 12:38:35 -0300

    Exercise 2

---
+++ Lista1/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days, months, years;
+            Console.WriteLine("Please, inform your age in days: ");
+            days = Convert.ToInt32(Console.ReadLine());
+
+            years = days / 365;
+            months = (days % 365) / 30;
+            days = (days % 365) % 30;
+
+            Console.WriteLine("You have " + years + " years, " + months + " months and " + days + " days!");
+        }
+    }
+}
