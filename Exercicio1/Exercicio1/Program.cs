01ccb4b82c27a070897a0d75c8b41ba39fd50a06
Felipe <felipedossantosflor@gmail.com>
2022-07-14 11:44:29 -0300

    Exercicio1

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int years, months, days, result; 
+            
+            Console.WriteLine("Please, type your age:");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type months: ");
+            months = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type days: ");
+            days = Convert.ToInt32(Console.ReadLine());
+
+            result = ((365 * years) + (months * 30) + days);
+
+            Console.WriteLine("Your age in days are: " + result);
+        }
+    }
+}
