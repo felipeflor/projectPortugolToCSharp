a73e8af32a0d6da39830f22088103ce0ff104a3c
Felipe <felipedossantosflor@gmail.com>
2022-07-15 08:42:35 -0300

    Exercicio

---
+++ Lista1/Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds, minutes, hours;
+            Console.WriteLine("Please, inform the seconds: ");
+            seconds = (Convert.ToInt32(Console.ReadLine()));
+
+            hours = seconds / 3600;
+            minutes = (seconds % 3600) / 60;
+            seconds = (seconds % 3600) % 60;
+
+            Console.WriteLine("Duration the event will be " + hours +
+                " hours " + minutes + " minutes " + "and " + seconds + " seconds");
+
+        }
+    }
+}
