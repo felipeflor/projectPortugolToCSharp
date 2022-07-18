800df8d448ad6195f5dbc29379b766ffa8e9bd88
Felipe <felipedossantosflor@gmail.com>
2022-07-18 08:27:03 -0300

    Exercicio 4

---
+++ Lista1/Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, r, s;
+            float d;
+
+            Console.WriteLine("Type the value of A: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type the value of B: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Type the value of C: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            r = (a + b) * (a + b);
+            s = (b + c) * (b + c);
+
+            d = (r + s) / 2;
+            Console.WriteLine(d);
+
+        }
+    }
+}
